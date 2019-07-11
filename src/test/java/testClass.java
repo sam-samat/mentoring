402fe10df6c08f74f6110a6d6b337e41bbcc2643
eldiiar <eldiar335@gmail.com>
2019-07-11 15:52:20 -0500

    Some text

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -1,8 +1,10 @@
 public class testClass {
     public static void main(String[] args) {
-        
+
         System.out.println("Welcome to my mentoring");
 
         System.out.println("Some text is here");
+
+        System.out.println("Just text");
     }
 }
