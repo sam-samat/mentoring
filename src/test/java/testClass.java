42977afd69b405e167f35c65de1c2f53083d81b7
mykhailoselivanov <michael777selivanov@gmail.com>
2019-07-11 15:41:05 -0500

    some changes

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -1,5 +1,7 @@
 public class testClass {
     public static void main(String[] args) {
+
         System.out.println("Welcome to my mentoring");
+        System.out.println("Awesome mentoring!");
     }
 }
